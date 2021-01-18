8c9955efe5e008a74df500e3287b01f6f5af608e
전예린 <031694@naver.com>
2021-01-18 10:28:53 +0900

    Initial commit

---
+++ app/src/main/java/Adapter/UserAdapter.java
@@ -0,0 +1,146 @@
+package Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatapp.MessageActivity;
+import com.example.chatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import model.Chat;
+import model.User;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean ischat;
+    String thealastmessage;
+
+    public UserAdapter(Context mcontext, List<User> mUsers, boolean ischat){
+        this.mContext = mcontext;
+        this.mUsers = mUsers;
+        this.ischat = ischat;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item, parent, false);
+        return new ViewHolder(view);
+    }
+    // inflation을 통해 메모리 객체화
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final User user = mUsers.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")) {
+            holder.profile_image.setImageResource(R.drawable.prof);
+        } else {
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+
+        if (ischat){
+            lastMessage(user.getId(),holder.last_msg);
+        }else {
+            holder.last_msg.setVisibility(View.GONE);
+        }
+
+        if (ischat){
+            if(user.getStatus().equals("Online")){
+                holder.img_on.setVisibility(View.VISIBLE);
+                holder.img_off.setVisibility(View.GONE);
+            }else {
+                holder.img_on.setVisibility(View.GONE);
+                holder.img_off.setVisibility(View.VISIBLE);
+            }
+        }else {
+            holder.img_on.setVisibility(View.GONE);
+            holder.img_off.setVisibility(View.GONE);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid",user.getId());
+                mContext.startActivity(intent);
+            }
+        });
+        //클릭하면 채팅창으로 이동
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public ImageView profile_image;
+        private ImageView img_on;
+        private ImageView img_off;
+        private TextView last_msg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            img_on = itemView.findViewById(R.id.img_on);
+            img_off = itemView.findViewById(R.id.img_off);
+            last_msg=itemView.findViewById(R.id.last_msg);
+        }
+    }
+
+    private void lastMessage(final String userid , final TextView last_msg){
+        thealastmessage = "default";
+        final FirebaseUser firebaseUser  = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Chats");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Chat chat = snapshot.getValue(Chat.class);
+                    if (chat.getReceiver().equals(firebaseUser.getUid()) && chat.getSender().equals(userid) || chat.getReceiver().equals(userid) &&
+                    chat.getSender().equals(firebaseUser.getUid())){
+                        thealastmessage = chat.getMessage();
+                    }
+                }
+                switch (thealastmessage){
+                    case "default" :
+                        last_msg.setText("No Message");
+                        break;
+
+                    default:
+                        last_msg.setText(thealastmessage);
+                        break;
+                }
+                thealastmessage = "default";
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
